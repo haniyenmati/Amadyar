508d200304a2e19cc975ad8baca288498ad06783
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-10 11:24:37 +0430

    urls added for retrieving driver Orders, uncompleted orders and order path estimation

---
+++ backend/haul/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from haul.views import OrderPathEstimationView, OrdersView, UncompletedOrdersView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('order/', OrdersView.as_view(), name='orders'),
+    path('order/uncompleted/', UncompletedOrdersView.as_view(), name='uncompleted_orders'),
+    path('order/<int:order_pk>/path/estimation/', OrderPathEstimationView.as_view(), name='order_path_estimation'),
+]
